e2f52eb2288d745c689cec15eaedcfa7c74568ab
Suman Roy <suman.roy@sourcebits.com>
2015-11-25 12:26:59 +0530

    Initial Commit

---
+++ Charting/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Charting
+//
+//  Created by Suman Roy on 25/11/15.
+//  Copyright (c) 2015 sourcebits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
